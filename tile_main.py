fa6954c39b71480861a481f83dbdc74436b49856
muratkendir <muratkendir@github.com>
2025-11-06 16:33:59 +0100

    images fixed

--- tile_main.py
+++ tile_main.py
@@ -13,15 +13,13 @@ from sql_blocks import *
 from kernel import set_kernel
 from pg_connection import create_materialized_view, run_query, index_materialized_view
 
-
-
 def tile(args):
     query = set_kernel()
     geom_col = query.select_elements[0].range_alias
     mv_name = "geometries"
     crt_mv = create_materialized_view(mv_name, str(query))
     ind_mv = index_materialized_view(mv_name, geom_col)
-    print(ind_mv)
+    #print(ind_mv)
     run_query(args, crt_mv)
     run_query(args, ind_mv)
     generate_tiles(args, mv_name, f"{geom_col}")
